5c231ba08a3d0796a768b5e1c761d1368ee50c12
Alexandre Juillard <ajuillard69@gmail.com>
2026-07-26 19:57:26 +0200

    [NOTICKET]chore: ajout spotbugs dans pom + configuration seuil Medium + corrections suite a

--- src/main/java/fr/stockshop/stock_api/user/entity/User.java
+++ src/main/java/fr/stockshop/stock_api/user/entity/User.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.io.Serial;
 import java.time.Instant;
 import java.util.Collection;
 import java.util.List;
@@ -39,6 +40,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 @AllArgsConstructor
 @Builder
 public class User implements UserDetails {
+
+  @Serial private static final long serialVersionUID = 1L;
 
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
